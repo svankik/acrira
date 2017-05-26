1fed5efd25975b3a3aa02e6f0f5e9506e9471500
Sylvain BRUYAS <sbruyas@ecedi.fr>
2017-05-26 16:35:19 +0200

    Adjust indents

--- wp-content/themes/acrira/functions.php
+++ wp-content/themes/acrira/functions.php
@@ -2,14 +2,14 @@
 add_action( 'wp_enqueue_scripts', 'acrira_enqueue_styles' );
 
 function acrira_enqueue_styles() {
-    $parent_style = 'twentyseventeen-style'; // This is 'twentyseventeen-style' for the Twenty Seventeen theme.
+	$parent_style = 'twentyseventeen-style'; // This is 'twentyseventeen-style' for the Twenty Seventeen theme.
 
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
-        wp_get_theme()->get('Version')
-    );
+	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'child-style',
+		get_stylesheet_directory_uri() . '/style.css',
+		array( $parent_style ),
+		wp_get_theme()->get('Version')
+	);
 }
 
 add_action( 'init', 'codex_cinema_init' );
