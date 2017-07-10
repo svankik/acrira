aeaa089b20f097511e8ce2f0144efb80a32ef64d
Sylvain BRUYAS <sbruyas@ecedi.fr>
2017-07-10 13:11:42 +0200

    Upgrade wordpress core to 4.8 and plugins

--- wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit705a1972ae70196fe3c15874d70a0ac9::getLoader();
+return ComposerAutoloaderInit85ff0426e49f198c57f62ac90c904ecd::getLoader();
