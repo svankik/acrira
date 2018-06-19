650313438515c3ddb1c32abf392803ed5a746a22
Sylvain BRUYAS <sbruyas@ecedi.fr>
2018-06-19 13:17:45 +0200

    Add gmap

--- wp-content/themes/acrira/tpl-cinemas-adherents.php
+++ wp-content/themes/acrira/tpl-cinemas-adherents.php
@@ -33,19 +33,74 @@ get_header(); ?>
 
 		</main><!-- #main -->
 
+		<?php
+
+		$cinemas = new WP_Query( 
+			array(
+				'post_type'      => 'cinema',
+				'category_name'  => 'cinemas-en-reseau',
+				'orderby'        => 'meta_value',
+				'meta_key'		 => 'ville',
+				'order'          => 'ASC',
+				'posts_per_page' => -1,
+			)
+		);
+
+		?>
+
+		<div class="entry-map">
+				
+			<div class="acf-map" style="width:100%; height: 400px;">
+
+				<?php
+
+				while ( $cinemas->have_posts() ) : $cinemas->the_post();
+
+					$map = get_field ( 'location' );
+
+				?>
+
+					<div class="marker" data-lat="<?php echo $map['lat']; ?>" data-lng="<?php echo $map['lng']; ?>">
+						
+						<div class="sr-only">
+
+							<h1 class="bold"><?php bloginfo( 'name' ); ?></h1>
+
+							<?php
+
+								$description = get_bloginfo( 'description', 'display' );
+
+								if ( $description || is_customize_preview() ) :
+								
+									?>
+										<p class="bold"><?php echo $description; ?></p>
+									<?php 
+
+								endif; 
+
+							?>	
+						
+						</div>
+
+						<div class="logo"><span class="icon-logo"></span></div>
+
+						<p class="map-address bold"><?php echo $map['address']; ?></p>
+
+					</div>
+				<?php
+
+				endwhile; // End of the loop.
+
+				?>
+			</div>
+		</div>
+
+
 		<div class="accordion col-md-12">
 		
 			<?php
-
-				$cinemas = new WP_Query( 
-					array(
-						'post_type'      => 'cinema',
-						'category_name'  => 'cinemas-en-reseau',
-						'orderby'        => 'title',
-						'order'          => 'ASC',
-						'posts_per_page' => -1,
-					)
-				);
+				
+				$cinemas->wp_rewind_posts();
 
 				while ( $cinemas->have_posts() ) : $cinemas->the_post();
 
