fb5b666f37d3c7c7c26b96263488c1b09cf5fc43
Sylvain Bruyas <sbruyas@ecedi.fr>
2018-10-28 10:00:48 +0100

    Add template for Page Espace Adhérents - Les films

---
+++ wp-content/themes/acrira/tpl-espace-adherents-films.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Template Name: Page Espace Adhérents - Les films
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+get_header(); ?>
+
+    <div class="container">
+
+        <div class="content-area row">
+
+            <main id="main" class="site-main col-md-12" role="main">
+
+              <?php
+              while ( have_posts() ) : the_post();
+
+                get_template_part( 'template-parts/page/content', 'page' );
+
+              endwhile; // End of the loop.
+              ?>
+
+            </main><!-- #main -->
+
+          <?php
+
+          $films = new WP_Query(
+            array(
+              'post_type'      => 'film',
+              'order'          => 'ASC',
+              'posts_per_page' => -1,
+            )
+          );
+
+          ?>
+
+            <div class="accordion col-md-12">
+
+              <?php
+
+              while ( $films->have_posts() ) : $films->the_post();
+
+                get_template_part( 'template-parts/post/content', 'film' );
+
+              endwhile; // End of the loop.
+
+              ?>
+
+            </div>
+
+        </div><!-- #primary -->
+
+    </div><!-- .wrap -->
+
+<?php get_footer();
