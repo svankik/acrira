ab516d061aef56f4e28e62bf4959c60e4cc7e8fa
JuZED <or.n.juz@gmail.com>
2018-11-20 09:43:02 +0100

    Archive Film Protection contre utilisateurs non connectés

--- wp-content/themes/acrira/archive-film.php
+++ wp-content/themes/acrira/archive-film.php
@@ -10,67 +10,77 @@
  * @version 1.0
  */
 
+$user = wp_get_current_user();
+
 get_header(); ?>
 
-<div class="wrap">
+<div class="container">
+	<div class="content-area row">
+		<main id="main" class="site-main col-md-12" role="main">
 
-	<?php if ( have_posts() ) : ?>
-		<header class="page-header">
-			<?php
-				$title        = '';
-				$introduction = '';
+			<?php if ( $user->ID ) : ?>
+				
+				<?php if ( have_posts() ) : ?>
+					<header class="page-header">
+						<?php
+							$title        = '';
+							$introduction = '';
 
-				if ( isset( $_GET['secteur'] ) ) {
-					switch($_GET['secteur']) {
-						case 'lyceens-et-apprentis-au-cinema':
-							$title = 'Les films';
-							$introduction = __( '%% lycéens et apprentis au cinema / films introduction', 'acrira' );
-							break;
-					}				
-				}
+							if ( isset( $_GET['secteur'] ) ) {
+								switch($_GET['secteur']) {
+									case 'lyceens-et-apprentis-au-cinema':
+										$title = 'Les films';
+										$introduction = __( '%% lycéens et apprentis au cinema / films introduction', 'acrira' );
+										break;
+								}				
+							}
 
-				if ( ! empty ( $title ) ) {
-					echo '<h1 class="page-title">'.$title.'</h1>';
-				}
+							if ( ! empty ( $title ) ) {
+								echo '<h1 class="page-title">'.$title.'</h1>';
+							}
 
-				echo $introduction;
-			?>
-		</header><!-- .page-header -->
-	<?php endif; ?>
+							echo $introduction;
+						?>
+					</header><!-- .page-header -->
+				<?php endif; ?>
 
-	<?php get_sidebar(); ?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-		<?php
-		if ( have_posts() ) : ?>
-			<div id="film-list">
 				<?php
-				/* Start the Loop */
-				while ( have_posts() ) : the_post();
 
-					/*
-					 * Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'template-parts/post/content', get_post_type() );
+					if ( have_posts() ) : ?>
+						<div id="film-list">
+							<?php
+							/* Start the Loop */
+							while ( have_posts() ) : the_post();
 
-				endwhile;
+								/*
+								 * Include the Post-Format-specific template for the content.
+								 * If you want to override this in a child theme, then include a file
+								 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+								 */
+								get_template_part( 'template-parts/post/content', get_post_type() );
 
-				the_posts_pagination( array(
-					'prev_text' => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous page', 'twentyseventeen' ) . '</span>',
-					'next_text' => '<span class="screen-reader-text">' . __( 'Next page', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
-					'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyseventeen' ) . ' </span>',
-				) ); ?>
-			</div>
-		<?php
-		else :
+							endwhile;
 
-			get_template_part( 'template-parts/post/content', 'none' );
+							the_posts_pagination( array(
+								'prev_text' => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous page', 'twentyseventeen' ) . '</span>',
+								'next_text' => '<span class="screen-reader-text">' . __( 'Next page', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
+								'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyseventeen' ) . ' </span>',
+							) ); ?>
+						</div>
+					<?php
+					else :
 
-		endif; ?>
+						get_template_part( 'template-parts/post/content', 'none' );
+
+					endif;
+				
+			else : 
+
+				get_template_part( 'template-parts/post/content', 'none' );
+
+			endif;
+
+			?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
