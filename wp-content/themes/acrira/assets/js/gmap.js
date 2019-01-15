316ab1e43604d34ea27291e74cdcc4438c4836c1
Sylvain Bruyas <sbruyas@ecedi.fr>
2019-01-15 10:28:23 +0100

    Put the gmap in plan mode

--- wp-content/themes/acrira/assets/js/gmap.js
+++ wp-content/themes/acrira/assets/js/gmap.js
@@ -222,8 +222,8 @@
 
 
 			//Associate the styled map with the MapTypeId and set it to display.
-			map.mapTypes.set('styled_map', styledMapType);
-			map.setMapTypeId('styled_map');
+			// map.mapTypes.set('styled_map', styledMapType);
+			// map.setMapTypeId('styled_map');
 			
 			
 			// add a markers reference
