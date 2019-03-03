37bc2a1d5c2bfd79574ecd617261894dd9a093f7
Sylvain Bruyas <sbruyas@ecedi.fr>
2019-03-03 17:57:54 +0100

    Add filters on films list

--- wp-content/themes/acrira/tpl-espace-adherents-films.php
+++ wp-content/themes/acrira/tpl-espace-adherents-films.php
@@ -37,22 +37,112 @@ get_header(); ?>
 
 			<?php
 
-				if ( $user->ID ) :
+				if ( $user->ID ) : ?>
 
-					$films = new WP_Query(
-						array(
-							'post_type'      => 'film',
-							'orderby'        => 'title',
-							'order'          => 'ASC',
-							'posts_per_page' => -1,
-						)
-					);
+                        <div class="filters-area">
 
-					?>
+                            <h2>Filtrer les films</h2>
+
+                            <?php
+
+                            $films = new WP_Query(
+	                            array(
+		                            'post_type'      => 'film',
+		                            'orderby'        => 'title',
+		                            'order'          => 'ASC',
+		                            'posts_per_page' => -1,
+	                            )
+                            );
+
+                            while ( $films->have_posts() ) : $films->the_post();
+
+	                            foreach(get_field( 'realisateurs' ) as $key => $realisateur) {
+		                            $realisateurs[] = $realisateur['realisateur'];
+                                }
+	                            foreach(get_field( 'genres' ) as $key => $genre) {
+		                            $genres[] = $genre['genre'];
+	                            }
+	                            foreach(get_field( 'origines' ) as $key => $origine) {
+		                            $origines[] = $origine['origine'];
+	                            }
+
+                            endwhile;
+
+                            $realisateurs = array_unique($realisateurs);
+                            sort($realisateurs);
+                            $genres = array_unique($genres);
+                            sort($genres);
+                            $origines = array_unique($origines);
+                            sort($origines);
+
+                            ?>
+
+                            <form name="films-filter" action="/espace-adherents-parlons-cinema" method="get">
+
+                            <span>Réalisateur :</span>
+                            <select name="realisateur">
+                                <option value="">Tous</option>
+                                <?php
+                                foreach ($realisateurs as $realisateur) {
+                                    print '<option value="' . $realisateur . '">' . $realisateur . '</option>';
+                                }
+                                ?>
+                            </select>
+
+                            <span>Genre :</span>
+                            <select name="genre">
+                                <option value="">Tous</option>
+		                        <?php
+		                        foreach ($genres as $genre) {
+			                        print '<option value="' . $genre . '">' . $genre . '</option>';
+		                        }
+		                        ?>
+                            </select>
+
+                            <span>Origine :</span>
+                            <select name="origine">
+                                <option value="">Toutes</option>
+		                        <?php
+		                        foreach ($origines as $origine) {
+			                        print '<option value="' . $origine . '">' . $origine . '</option>';
+		                        }
+		                        ?>
+                            </select>
+
+                            <span>Jeune public :</span>
+                            <input type="checkbox" name="type_public" value="1" />
+
+                            <input type="submit" value="Filtrer" />
+
+                            </form>
+                        </div>
+
+                        <h2>Les films</h2>
 
 						<div class="accordion col-md-12">
 
 							<?php
+
+							if(!empty($_GET['realisateur'])) {
+
+							}
+
+							if(!empty($_GET['genre'])) {
+
+							}
+
+							if(!empty($_GET['origine'])) {
+
+							}
+
+							$films = new WP_Query(
+								array(
+									'post_type'      => 'film',
+									'orderby'        => 'title',
+									'order'          => 'ASC',
+									'posts_per_page' => -1,
+								)
+							);
 
 							while ( $films->have_posts() ) : $films->the_post();
 
